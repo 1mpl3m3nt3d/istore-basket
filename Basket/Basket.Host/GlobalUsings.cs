c91da9a9b009e0e1eaff88bd0e946bed02248815
1mpl3m3nt3d <105541001+1mpl3m3nt3d@users.noreply.github.com>
2022-09-26 00:37:28 +0300

    Up

--- Basket/Basket.Host/GlobalUsings.cs
+++ Basket/Basket.Host/GlobalUsings.cs
@@ -7,6 +7,7 @@ global using Infrastructure;
 global using Infrastructure.Extensions;
 global using Infrastructure.Filters;
 global using Infrastructure.Identity;
+global using Infrastructure.Services;
 global using Infrastructure.Services.Interfaces;
 
 global using Microsoft.AspNetCore.Authorization;
--- Basket/Basket.Host/Program.cs
+++ Basket/Basket.Host/Program.cs
@@ -2,11 +2,11 @@ using Basket.Host.Configurations;
 using Basket.Host.Services;
 using Basket.Host.Services.Interfaces;
 
-using Infrastructure.Services;
+var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
 var configuration = GetConfiguration();
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions() { ContentRootPath = baseDirectory });
 
 builder.Services
     .AddControllers(options => options.Filters.Add(typeof(HttpGlobalExceptionFilter)))
@@ -55,80 +55,22 @@ builder.Services.AddTransient<IRedisCacheConnectionService, RedisCacheConnection
 builder.Services.AddTransient<ICacheService, CacheService>();
 builder.Services.AddTransient<IBasketService, BasketService>();
 
-builder.Services.AddCors(options => options.AddPolicy(
-        "CorsPolicy",
-        builder => builder
-            .SetIsOriginAllowed((host) => true)
-            .WithOrigins(configuration["SpaUrl"], configuration["PathBase"], configuration["GlobalUrl"], configuration["Authorization:Authority"])
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .AllowCredentials()));
+builder.Services.AddCors(
+    options => options
+        .AddPolicy(
+            "CorsPolicy",
+            builder => builder
+                .SetIsOriginAllowed((host) => true)
+                .WithOrigins(
+                    configuration["Authorization:Authority"],
+                    configuration["GlobalUrl"],
+                    configuration["PathBase"],
+                    configuration["SpaUrl"])
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()));
 
-if (configuration["Nginx:UseNginx"] == "true" || Environment.GetEnvironmentVariable("Nginx__UseNginx") == "true")
-{
-    try
-    {
-        if (configuration["Nginx:UseInitFile"] == "true" || Environment.GetEnvironmentVariable("Nginx__UseInitFile") == "true")
-        {
-            var initFile = configuration["Nginx:InitFilePath"] ?? Environment.GetEnvironmentVariable("Nginx__InitFilePath") ?? "/tmp/app-initialized";
-
-            if (!File.Exists(initFile))
-            {
-                File.Create(initFile).Close();
-            }
-
-            File.SetLastWriteTimeUtc(initFile, DateTime.UtcNow);
-        }
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Environment variable <Nginx__UseNginx> is set to 'true', but there was an exception while configuring Initialize File:\n{ex.Message}");
-    }
-
-    try
-    {
-        if (configuration["Nginx:UseUnixSocket"] == "true" || Environment.GetEnvironmentVariable("Nginx__UseUnixSocket") == "true")
-        {
-            var unixSocket = configuration["Nginx:UnixSocketPath"] ?? Environment.GetEnvironmentVariable("Nginx__UnixSocketPath") ?? "/tmp/nginx.socket";
-
-            builder.WebHost.ConfigureKestrel(kestrel => kestrel.ListenUnixSocket(unixSocket));
-        }
-        else
-        {
-            var portParsed = int.TryParse(configuration["PORT"] ?? Environment.GetEnvironmentVariable("PORT"), out var port);
-
-            if (portParsed)
-            {
-                builder.WebHost.ConfigureKestrel(kestrel => kestrel.ListenAnyIP(port));
-            }
-        }
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Environment variable <Nginx__UseNginx> is set to 'true', but there was an exception while configuring Kestrel:\n{ex.Message}");
-    }
-}
-else
-{
-    var portEnv = configuration["PORT"] ?? Environment.GetEnvironmentVariable("PORT");
-
-    try
-    {
-        if (portEnv != null)
-        {
-            var portParsed = int.TryParse(portEnv, out var port);
-
-            if (portParsed)
-            {
-                builder.WebHost.ConfigureKestrel(kestrel => kestrel.ListenAnyIP(port));
-            }
-        }
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Environment variable <PORT> is set to '{portEnv}', but there was an exception while configuring Kestrel:\n{ex.Message}");
-    }
-}
+builder.AddNginxConfiguration();
 
 var app = builder.Build();
 
@@ -158,7 +100,7 @@ app.Run();
 IConfiguration GetConfiguration()
 {
     var builder = new ConfigurationBuilder()
-        .SetBasePath(Directory.GetCurrentDirectory())
+        .SetBasePath(baseDirectory)
         .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
         .AddEnvironmentVariables()
         .AddCommandLine(args);
